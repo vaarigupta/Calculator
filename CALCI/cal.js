dc558806ff2590a9c247d9d5db1b8c66f1cbad49
vaarigupta <vaarigupta24@gmail.com>
2018-06-12 16:00:50 +0530

    calci added

---
+++ CALCI/cal.js
@@ -0,0 +1,21 @@
+var exp ="";
+
+function handleclick(btnval)
+{
+	if(btnval ==="=") /// === is used to compare type and value of the expression given 
+	{
+      exp = new String(eval(exp)); // is a special function used for evaluating strings and give results and parse them 
+	}
+	else if (btnval==="C")
+	{
+		exp="";
+	}
+	else
+	{ exp += btnval;
+
+	}
+  var display = document.getElementById("display");
+ display.innerText = exp;
+}
+///innerText is a combination of html n js and applied on the text in the div element
+
