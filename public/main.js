705da8e8386b729eb48317c2b7a946e7ee2cd4c0
vaarigupta <vaarigupta24@gmail.com>
2019-02-15 22:53:15 +0530

    modified

--- public/main.js
+++ public/main.js
@@ -2,9 +2,9 @@ var exp ="";
 
 function handleclick(btnval)
 {
-	if(btnval ==="=") /// === is used to compare type and value of the expression given 
+	if(btnval ==="=") /// === is used to compare type and value of the expression given
 	{
-      exp = new String(eval(exp)); // is a special function used for evaluating strings and give results and parse them 
+      exp = new String(eval(exp)); // is a special function used for evaluating strings and give results and parse them
 	}
 	else if (btnval==="C")
 	{
@@ -18,4 +18,3 @@ function handleclick(btnval)
  display.innerText = exp;
 }
 ///innerText is a combination of html n js and applied on the text in the div element
-
