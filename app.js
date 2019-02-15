62d06cf0f0c3fdd1e14495dce8a036d449461070
vaarigupta <vaarigupta24@gmail.com>
2019-02-15 21:38:00 +0530

    Calci BACKEND

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/CALCI'));
 
+// app.use('/',(req,res,next)=>{
+//   next();
+// })
+app.use(express.static(__dirname + '/CALCI'));
 app.listen(3000,()=>{
   console.log("Server running at Port 3000");
 })
